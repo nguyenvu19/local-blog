d81c9707ff67263000cec35319cf40f4d982a8ba
Nguyen Vu <nguyenvu191000@gmail.com>
2023-02-12 10:06:50 +0700

    prepare callback

--- src/pages/blog/blog.reducer.ts
+++ src/pages/blog/blog.reducer.ts
@@ -1,4 +1,4 @@
-import { createAction, createReducer } from '@reduxjs/toolkit'
+import { createAction, createReducer, nanoid } from '@reduxjs/toolkit'
 import { initialPostList } from 'constants/blog'
 import { Post } from 'types/blog.type'
 
@@ -13,7 +13,14 @@ const initialState: BlogState = {
 }
 
 // Action
-export const addPost = createAction<Post>('blog/addPost')
+export const addPost = createAction('blog/addPost', function (post: Omit<Post, 'id'>) {
+  return {
+    payload: {
+      ...post,
+      id: nanoid()
+    }
+  }
+})
 export const deletePost = createAction<string>('blog/deletePost')
 export const startEditingPost = createAction<string>('/blog/startEditingPost')
 export const cancelEditingPost = createAction('/blog/cancelEditingPost')
